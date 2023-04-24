18cdd8787c73a83b5c7f37c6706d346b563b9021
Janek Tuisk <38907917+jTuisk@users.noreply.github.com>
2023-04-24 18:10:39 +0300

    Backup

--- Survival Game/Assets/Player/Scripts/Items/UsableItem.cs
+++ Survival Game/Assets/Player/Scripts/Items/UsableItem.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace Game.Player.Item
 {
     [System.Serializable]
     public class UsableItem : Item
     {
-        
+        public UnityEvent effect;
+
+        public void UseItem()
+        {
+            effect.Invoke();
+        }
     }
 }
---
+++ Survival Game/Assets/Player/Scripts/Items/UsableItemEffects.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player.Item
+{
+    public class UsableItemEffects : MonoBehaviour
+    {
+
+
+    }
+}
